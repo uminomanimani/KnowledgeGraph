8ef6d1ab3597de94733722db0d7a039264ca940f
uminomanimani <ma_likai@icloud.com>
2023-06-23 13:13:07 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    batch_size = 512
+    max_seq_len = 50
+    learning_rate = 1e-5
+    epoch = 200
+    hidden_size = 1024
+    weight_decay = 0
+    path_bert = "./Bert/"
+    num_entities = 9
